0c8cb91ea4335a002e5dea22e29d22bebeba3edf
Marlon Cueva <marlon.cueva@talma.com.ec>
2025-05-22 20:36:20 -0500

    sincronizacion

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 
 # URL = "http://localhost:8080/predict/"
-URL = "https://model-flask-docker-an73.onrender.com/predict/"Buen trabajo""
+URL = "https://model-flask-docker-an73.onrender.com/predict/"
